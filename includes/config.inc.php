de01df7b2f3a20cde83f9987d8cc2de1b78c2d06
gyel123 <vizes35@gmail.com>
2024-05-06 12:27:19 +0200

    includes

---
+++ includes/config.inc.php
@@ -0,0 +1,35 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Trackmaster Logistics Kft.',
+);
+
+$fejlec = array(
+    'kepforras' => '',
+    'kepalt' => 'logo',
+	'cim' => 'Trackmaster Logistics Kft.',
+	'motto' => 'Track your Truck!'
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Trackmaster Logistics Kft.'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),	
+	'kepfeltoltes' => array('fajl' => 'kepfeltoltes', 'szoveg' => 'Képfeltöltés', 'menun' => array(1,1)),
+    	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'e-mail adatok' => array('fajl' => 'e-mail adatok', 'szoveg' => 'E-mail adatok', 'menun' => array(1,1)),
+	'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+   	'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),	
+    	'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    	'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+$MAPPA = './images/';
+$TIPUSOK = array ('.jpg', '.png');
+$MEDIATIPUSOK = array('image/jpeg', 'image/png');
+$DATUMFORMA = "Y.m.d. H:i";
+$MAXMERET = 500*1024;
+?>
