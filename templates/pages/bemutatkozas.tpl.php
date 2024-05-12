8ab63dbf58a65714b185ab80ddbd2e6724ee9497
gyel123 <vizes35@gmail.com>
2024-05-12 23:08:40 +0200

    módosítás

--- templates/pages/bemutatkozas.tpl.php
+++ templates/pages/bemutatkozas.tpl.php
@@ -7,3 +7,41 @@ Próbálja ki, nem fogja megbánni.</p>
 <video width="420" height="315" controls>
 <source src="video/volvo.mp4" type="video/mp4">
 </video>
+<!--GPS-->
+<p>Az aktuális helyzeted GPS koordinátája</p>
+<p>Hasonló elven működik a gépkocsik aktuális helyzetlekérése:</p>
+<button onclick="getLocation()">Próbáld ki, klikkelj ide</button>
+<p id="demo"></p>
+
+<script>
+    var x = document.getElementById("demo");
+    
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            x.innerHTML = "Ez a böngésző nem támogatja a földrajzi helymeghatározást.";
+        }
+    }
+
+    function showPosition(position) {
+        x.innerHTML = "Szélesség: " + position.coords.latitude + "<br> Hosszúság: " + position.coords.longitude;
+    }
+
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "Felhasználó elutasította a helymeghatározást.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Nem lehet meghatározni a helyet.";
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "A helymeghatározás túllépte az időkorlátot.";
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "Ismeretlen hiba történt a helymeghatározás során.";
+                break;
+        }
+    }
+</script>
